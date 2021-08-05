3012d2b164845b274049132a64789aeb8be24e0b
heyzec <hz.tiang@gmail.com>
2021-08-05 12:37:27 +0800

    Added composer

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '9b8e0d8e1cc32dca26722f929deed6b4dfddbdf0',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '9b8e0d8e1cc32dca26722f929deed6b4dfddbdf0',
+            'dev_requirement' => false,
+        ),
+    ),
+);
