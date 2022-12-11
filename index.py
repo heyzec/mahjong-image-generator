cdd37b48e1d1935aca8578fe56720231bd875ec2
heyzec <61238538+heyzec@users.noreply.github.com>
2022-12-11 21:36:18 +0800

    refactor: remove need to use tempfiles

--- index.py
+++ index.py
@@ -1,6 +1,7 @@
+from io import BytesIO
+
 from flask import Flask, request, send_file
 from PIL import Image
-import shutil
 
 app = Flask(__name__)
 
@@ -20,59 +21,29 @@ def result():
         else:
             raise Exception()
 
-    first_run = True
-    tempfile = "tempfile.png"
+
+    imgs = []
     for tile in output:
         filename = f"tiles/{tile}.png"
-        if first_run:
-            shutil.copyfile(filename, tempfile)
-            first_run = False
-        else:
-            merge_images(tempfile, filename)
+        imgs.append(Image.open(filename))
 
-    return send_file(tempfile, mimetype='image/png')
+    total_width = sum(map(lambda img: img.size[0], imgs))
+    max_height = max(map(lambda img: img.size[1], imgs))
 
-def merge_images(filename1, filename2):
-    img1, img2 = Image.open(filename1), Image.open(filename2)
-
-    total_width = sum([img1.size[0], img2.size[0]])
-    max_height = max([img1.size[1], img2.size[1]])
-
-    new_im = Image.new('RGB', (total_width, max_height))
+    new_img = Image.new('RGB', (total_width, max_height))
 
     x_offset = 0
-    new_im.paste(img1, (x_offset, 0))
-    new_im.paste(img2, (img1.size[0], 0))
+    for img in imgs:
+        new_img.paste(img, (x_offset, 0))
+        x_offset += img.size[0]
 
-    new_im.save(filename1)
+    img_io = BytesIO()
+    new_img.save(img_io, 'PNG')
+    img_io.seek(0)
+
+    return send_file(img_io, mimetype='image/png')
+
 
 if __name__ == '__main__':
     app.run(debug=True, host="0.0.0.0")
-
-# for ($i = 0; $i < strlen($q); $i++) {
-# 	if (is_numeric($q[$i])) {
-# 		$temp[] = $q[$i];
-# 	} else if (strpos('mpsz', $q[$i]) !== false) {
-# 		foreach ($temp as $j) {
-# 			$output[] = $j . $q[$i];
-# 		}
-# 		$temp = array();
-# 	} else {
-# 		echo "ERROR";
-# 	}
-# }
-
-
-# $firstRun = true;
-# foreach ($output as $tile) {
-# 	$filename = "tiles/".$tile.".png";
-# 	if ($firstRun) {
-# 		copy($filename, "temp_".$q."png");
-# 		$firstRun = false;
-# 	} else {
-# 		mergeImages("temp_".$q."png", $filename);
-# 	}
-# }
-
-
 
